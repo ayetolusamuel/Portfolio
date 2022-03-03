621e0cdd8fdf32b2e9364fb639f052e558659d3e
ayetolusamuel <ayetolusamuel@gmail.com>
2022-03-03 12:32:07 +0100

    init commit

---
+++ cloud-function/functions/index.js
@@ -0,0 +1,44 @@
+const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
+
+const mailTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: "codingwithset@gmail.com",
+    pass: "Setonji04$",
+  },
+});
+
+// Sends an email confirmation when a user create account on the app.
+exports.sendEmailConfirmation = functions.database.ref('/users/').onWrite(async (change) => {
+
+    const snapshot = change.after;
+    const val = snapshot.val();
+    console.log("Name::::: "+val.name)
+    console.log("Email::: "+val.email)
+    console.log("Message:: "+val.message)
+
+   const mailOptions = {
+     from: '"Portfolio" <ayetolusamuel@gmail.com>',
+     to: val.email,
+   };
+
+    // Building Email message.
+   mailOptions.subject =   "Portfolio Notification";
+   mailOptions.html =  `
+   <img src="https://bit.ly/2UAujkP" alt="Girl in a jacket" style="width:300px;height:300px;">
+   <h1><br>Thanks for reaching out to us.  </h1>
+   
+   <b><br><i>Name: ${val.name} </b></i>
+   <b><br><i>Email: ${val.email} </b></i>
+   <b><br><i>Message: ${val.message} </b></i>
+   <p>our goal is to serve you better.</p> `;
+
+   try {
+     await mailTransport.sendMail(mailOptions);
+     console.log(`New ${val.email} confirmation email sent to:`, val.email);
+   } catch(error) {
+     console.error('There was an error while sending the email:', error);
+   }
+   return null;
+ });
