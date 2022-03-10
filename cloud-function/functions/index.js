ada8a4790bacf176faf3897a42b7c645d2c2fe5d
ayetolusamuel <ayetolusamuel@gmail.com>
2022-03-10 11:22:17 +0100

    init commit

--- cloud-function/functions/index.js
+++ cloud-function/functions/index.js
@@ -2,45 +2,59 @@ const functions = require('firebase-functions');
 const nodemailer = require('nodemailer');
 const recipientEmail = "ayetolusamuel@gmail.com"
 
-
-const mailTransport = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: "codingwithset@gmail.com",
-    pass: "Setonji04$",
-  },
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: "codingwithset@gmail.com",
+        pass: "Setonji04$",
+    },
 });
 
-// Sends an email confirmation when a user create account on the app.
-exports.sendEmailConfirmation = functions.database.ref('/users/').onWrite(async (change) => {
+exports.sendMultipleEmail = functions.database.ref('/users/').onWrite(async(change) => {
 
     const snapshot = change.after;
-    const val = snapshot.val();
-    console.log("Name::::: "+val.name)
-    console.log("Email::: "+val.email)
-    console.log("Message:: "+val.message)
-   
-   const mailOptions = {
-     from: '"Portfolio" <ayetolusamuel@gmail.com>',
-     to: recipientEmail,
-   };
-
-    // Building Email message.
-   mailOptions.subject =   "Portfolio Notification";
-   mailOptions.html =  `
-   <img src="https://bit.ly/2UAujkP" alt="Girl in a jacket" style="width:300px;height:300px;">
-   <h1><br>Thanks for reaching out to us.  </h1>
-   
-   <b><br><i>Name: ${val.name} </b></i>
-   <b><br><i>Email: ${val.email} </b></i>
-   <b><br><i>Message: ${val.message} </b></i>
-   <p>our goal is to serve you better.</p> `;
-
-   try {
-     await mailTransport.sendMail(mailOptions);
-     console.log(`New ${recipientEmail} confirmation email sent to:`, recipientEmail);
-   } catch(error) {
-     console.error('There was an error while sending the email:', error);
-   }
-   return null;
- });
+        const result = snapshot.val();
+        console.log("Result "+result);
+    
+        const mailOptions = {
+            from: '"Portfolio" <ayetolusamuel@gmail.com>',
+            to: recipientEmail,
+            subject: "Portfolio Notification",
+            html: `<img src="https://bit.ly/2UAujkP" alt="Girl in a jacket" style="width:300px;height:300px;">
+                <h1><br>Thanks for reaching out to us.  </h1>
+            
+               <b><br><i>Name: ${result.name} </b></i>
+               <b><br><i>Email: ${result.email} </b></i>
+               <b><br><i>Message: ${result.message} </b></i>
+               <p>Our goal is to serve you better.</p> `,
+          };
+          
+          const mailOptions2 = {
+            from: '"Portfolio" <ayetolusamuel@gmail.com>',
+            to: result.email,
+            subject: "Portfolio Notification",
+            html: `
+  
+               <h1><br>Thanks for reaching out to us.  </h1>
+               
+               <b><br>Your mail is received, and acknowledged.</b></i>
+              
+               <p>Our goal is to serve you better.</p> `,
+          };
+          
+          Promise.all([
+            transporter.sendMail(mailOptions),
+            transporter.sendMail(mailOptions2),
+          ])
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err));
+        
+try {
+        const mails = await findAll();
+        const mailerPromises = mails.map((mail) => transporter.sendMail(mail.dataValues));
+        const responses = await Promise.all(mailerPromises);
+        console.log(responses, "All Mails Have Been Sent Successfully");
+      } catch (e) {
+        console.log(e);
+      }
+});
